2d198073364f6f691ced8cbbcf4e1ff86ef31790
elifksc <eekuscu14@gmail.com>
2024-11-01 14:38:38 +0300

    rergege

--- Readio.Core/Model/Entity/BaseEntity.cs
+++ Readio.Core/Model/Entity/BaseEntity.cs
@@ -5,4 +5,5 @@ public abstract class BaseEntity<TId>
     public TId Id { get; private set; } = default!;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
 }
